c37c8e89865e5679b249f1e0af63241c1ddc8f8e
Manrique Logan <manrique@aseba.org>
2016-11-03 11:13:41 -0700

    Add project files.

---
+++ src/OpenGameListWebApp/Scripts/app/app.component.ts
@@ -0,0 +1,8 @@
+﻿import {Component} from "@angular/core";
+
+@Component({
+    selector: "opengamelist",
+    template: `<h1>OpenGameList</h1><div>Work in progress...</div>`
+})
+
+export class AppComponent { } 
